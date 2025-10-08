048edaf9ea7b635d1c1d5fbf24aebbe609c35e82
tuanpham21105 <tuan.phamminh936@gmail.com>
2025-10-08 22:15:43 +0700

    add: Day07Lab DataAccess

---
+++ LTW_Day07Lab/LTW_Day07_TH/Models/QuanTri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LTW_Day07_TH.Models;
+
+public partial class QuanTri
+{
+    public int Id { get; set; }
+
+    public string? TaiKhoan { get; set; }
+
+    public string? MatKhau { get; set; }
+
+    public byte? TrangThai { get; set; }
+}
---
+++ LTW_Day07Lab/LTW_Day07_TH/Program.cs
@@ -0,0 +1,29 @@
+using LTW_Day07_TH.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<K64cnt2dbContext>(x => x.UseSqlServer(connectionString));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
